3646ba48fa8361eef7362b88c9c56225a915b91f
h_hisano <h_hisano@hisanon.local>
2020-07-03 00:46:05 +0900

    サイト作成

--- src/components/header.js
+++ src/components/header.js
@@ -1,32 +1,38 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import './header.scss'
+import "./style.css"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
+  <header>
+    <div className="head">
+      <h1>
+        <Link className="title" to="/">
           {siteTitle}
         </Link>
       </h1>
+      <span className="icon-Twitter_Social_Icon_Circle_White"></span>
+      <div className="test">
+        <ul className="menu">
+            <li className="menu__single">
+                <a href="#" className="init-bottom">NODENS</a>
+                <ul className="menu__second-level">
+                    <li><a href="/">VEGA</a></li>
+                    <li><a href="#">JOE</a></li>
+                </ul>
+            </li>
+        </ul>
+        <ul className="menu">
+            <li className="menu__single">
+                <a href="#" className="init-bottom">C</a>
+                <ul className="menu__second-level">
+                    <li><a href="/">WORLD</a></li>
+                    <li><a href="#">CHARACTER</a></li>
+                </ul>
+            </li>
+        </ul>
+      </div>
     </div>
   </header>
 )
@@ -36,7 +42,7 @@ Header.propTypes = {
 }
 
 Header.defaultProps = {
-  siteTitle: ``,
+  siteTitle: '',
 }
 
 export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,24 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+
+import "./index.scss"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div className="title">
+      <div className="neon">
+        HOTEL <span>NO</span>DENS
+      </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <div>
+      <blockquote class="twitter-tweet" data-theme="dark"><p lang="ja" dir="ltr">こちらの企画【<a href="https://twitter.com/H_NODENS_TL?ref_src=twsrc%5Etfw">@H_NODENS_TL</a>】に参加させていただきます！盗人兄弟です！関係等真っ白ですので是非遊んでください！<a href="https://twitter.com/hashtag/NODENS_CS?src=hash&amp;ref_src=twsrc%5Etfw">#NODENS_CS</a><a href="https://twitter.com/hashtag/NODENS_%E5%88%A9%E7%94%A8%E8%80%85?src=hash&amp;ref_src=twsrc%5Etfw">#NODENS_利用者</a> <a href="https://t.co/3MFsnPclFR">pic.twitter.com/3MFsnPclFR</a></p>&mdash; hirohisa (@hirohisasa) <a href="https://twitter.com/hirohisasa/status/1265216386831224832?ref_src=twsrc%5Etfw">May 26, 2020</a></blockquote>
+    </div>
+    <div className="timeline">
+      <a class="twitter-timeline" data-width="500" data-theme="dark" href="https://twitter.com/hirohisasa/timelines/1278567761124327424?ref_src=twsrc%5Etfw">test - Curated tweets by hirohisasa</a>
+    </div>
   </Layout>
 )
 
